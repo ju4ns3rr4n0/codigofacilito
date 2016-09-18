b76073cf9b05dcbcc4759a33c90f7241d4daada5
ju4ns3rr4n0 <ju4ns3rr4n0@gmail.com>
2016-09-18 21:00:20 +0200

    añadidas vistas nuevo y listar

--- refugio/apps/adopcion/models.py
+++ refugio/apps/adopcion/models.py
@@ -8,3 +8,7 @@ class Persona(models.Model):
 	telefono = models.CharField(max_length=12)
 	email = models.EmailField()
 	domicilio = models.TextField()
+
+	def __str__(self):
+		return '{} {}'.format(self.nombre, self.apellidos)
+		
--- refugio/apps/mascota/urls.py
+++ refugio/apps/mascota/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from apps.mascota.views import index
+from apps.mascota.views import index, mascota_view, mascota_list
 
 urlpatterns = [
-    url(r'^$', index),   
+    url(r'^$', index, name='index'),
+    url(r'^nuevo$', mascota_view, name='mascota_crear'),
+    url(r'^listar$', mascota_list, name='listar'),
 ]
