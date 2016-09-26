e3e0405864c9746b7f13987eeccc26ebdec5ffc7
ju4ns3rr4n0 <ju4ns3rr4n0@gmail.com>
2016-09-26 23:01:11 +0200

    finalizado crud

--- refugio/apps/adopcion/urls.py
+++ refugio/apps/adopcion/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
-from apps.adopcion.views import index
+from apps.adopcion.views import index, SolicitudList, SolicitudCreate, SolicitudUpdate, SolicitudDelete
 
 urlpatterns = [
-    url(r'^index$', index),   
+    url(r'^index$', index),
+    url(r'^listar$', SolicitudList.as_view(), name='solicitud_listar'), 
+    url(r'^nueva$', SolicitudCreate.as_view(), name='solicitud_crear'), 
+    url(r'^editar/(?P<pk>\d+)/$', SolicitudUpdate.as_view(), name='solicitud_editar'), 
+    url(r'^eliminar/(?P<pk>\d+)/$', SolicitudDelete.as_view(), name='solicitud_eliminar'), 
 ]
